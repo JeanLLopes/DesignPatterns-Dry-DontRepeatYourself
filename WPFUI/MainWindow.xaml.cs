0cb1bcb93a8f48108353c521ea97697b4b9c5428
jlopes <jean.llopes@outlook.com>
2018-11-03 12:58:50 -0300

    Test WPF OK

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,8 @@ namespace WPFUI
 
         private void createEmployeeIdButton_Click(object sender, RoutedEventArgs e)
         {
-
+            EmployeeProcessor employeeProcessor = new EmployeeProcessor();
+            employeeId.Text = employeeProcessor.GenerateEmployeeID(firstName.Text, lastName.Text);
         }
     }
 }
