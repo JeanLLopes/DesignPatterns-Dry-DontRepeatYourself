112e3a6c76cead38ed1365a9a057119321b40ed8
jlopes <jean.llopes@outlook.com>
2018-11-03 13:20:55 -0300

    Add Test Project

---
+++ LibraryTest/EmployeeProcessorTest.cs
@@ -0,0 +1,25 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace LibraryTest
+{
+    public class EmployeeProcessorTest
+    {
+        [Theory]
+        [InlineData("Jean", "Lopes", "JeanLope")]
+        public void GenerateEmploeeId_ShouldCalculate(string firstName, string secondName, string expectedStart)
+        {
+            //Arrange
+            EmployeeProcessor employeeProcessor = new EmployeeProcessor();
+            //Act
+            string actualStart = employeeProcessor.GenerateEmployeeID(firstName, secondName).Substring(0, 8);
+
+            //Assert
+            Assert.Equal(expectedStart, actualStart);
+        }
+    }
+}
